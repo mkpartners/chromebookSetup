5ffa56f7728ff7804d484d040e3168f270cf90eb
Matt Kaufman <the1mattkaufman@gmail.com>
2021-11-18 09:13:38 -0800

    Update install.sh

--- node/install.sh
+++ node/install.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 ### install node
-. ~/.nvm/nvm.sh
 echo BEGIN installing node
 echo ""
 echo nvm install node
 nvm install node
 echo ""
-echo ""
-echo ""
-echo ""
 echo nvm ls
 nvm ls
 echo ""
 echo END installing node
-echo ""
-echo ""
 echo ""
 echo ""
