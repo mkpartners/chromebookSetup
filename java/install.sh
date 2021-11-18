e3ace8b095eed1303b093b69c47b03c7464e4baa
Matt Kaufman <the1mattkaufman@gmail.com>
2021-11-18 09:05:17 -0800

    Updated per website

--- java/install.sh
+++ java/install.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
 ### install java
 echo BEGIN installings java
-echo ""
-echo "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
 
-echo ""
-echo "sudo echo 'deb http://repos.azulsystems.com/debian stable main' > /etc/apt/sources.list.d/zulu.list"
-echo 'deb http://repos.azulsystems.com/debian stable main' > zulu.list
-sudo mv zulu.list /etc/apt/sources.list.d/zulu.list
-echo ""
-echo "sudo apt-get update"
+# install the necessary dependencies
+sudo apt-get -q update
+sudo apt-get -yq install gnupg curl 
+
+# add Azul's public key
+sudo apt-key adv \
+  --keyserver hkp://keyserver.ubuntu.com:80 \
+  --recv-keys 0xB1998361219BD9C9
+
+# download and install the package that adds 
+# the Azul APT repository to the list of sources 
+curl -O https://cdn.azul.com/zulu/bin/zulu-repo_1.0.0-3_all.deb
+
+# install the package
+sudo apt-get install ./zulu-repo_1.0.0-3_all.deb
+
+# update the package sources
 sudo apt-get update
-echo ""
-echo "sudo apt-get install zulu-11"
-sudo apt-get -y install zulu-11
-echo ""
-echo "export JAVA_HOME=/usr/lib/jvm/zulu-11/"
+
+# install Azul Zulu JDK 11
+sudo apt-get install zulu11-jdk
+
 export JAVA_HOME=/usr/lib/jvm/zulu-11/
-# echo sudo apt-get install -y default-jdk
-# sudo apt-get install -y default-jdk
-# echo ""
-# echo "sudo apt-get install -f"
-# sudo apt-get install -f
-# echo ""
-# echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre"
-# export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
-# echo ""
+
+java -version
+
 echo END installing java
 echo ""
 echo ""
