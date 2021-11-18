c726f67b32586fdfcf9a020a1b91b9464b4c8066
Matt Kaufman <the1mattkaufman@gmail.com>
2021-11-18 09:12:38 -0800

    add nvm dir to path

--- nvm/install.sh
+++ nvm/install.sh
@@ -5,6 +5,7 @@ echo ""
 echo "sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash"
 sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash
 echo ""
+export NVM_DIR="$HOME/.config/nvm"
 echo ""
 echo END installing nvm
 echo ""
